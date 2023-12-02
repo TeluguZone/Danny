470b4cf53c1bbf3eb3e9889a77fcc40a00dc46fb
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2023-12-03 00:05:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/TeluguZone/Danny
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Danny
 fi
-cd /VJ-FILTER-BOT 
+cd /Danny
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
